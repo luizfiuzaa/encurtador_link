9539f223b9c9af182b45c9be308099c5cac5460a
luizfiuzaa <luizffreitas2006@gmail.com>
2024-04-17 18:01:02 -0300

    #

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    public function generate(){
+        return View('site.generate');
+    }
+    public function remove(){
+        return View('site.remove');
+    }
+    public function list(){
+        return View('site.list');
+    }
+}
